7a4804659e40226db3c7025bed8052e714062b59
Безобразов Григорий <gabezobrazov@sbercloud.ru>
2023-03-17 20:13:38 +0300

    axample about Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
+import React, { useState, useContext } from 'react';
 import './App.css';
 
+const FirstContext = React.createContext()
+const SecondContext = React.createContext()
+
+const CounterComponent = () => {
+  const { conter, inctrement, dicrement } = useContext(FirstContext)
+
+  return (
+    <div>
+      <p>{conter}</p>
+      <button onClick={inctrement}>+</button>
+      <button onClick={dicrement}>-</button>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <header>Хедер</header>
+      <div>
+        <CounterComponent />
+      </div>
+    </div>
+  )
+}
+
+const Footer = () => {
+  const { exampleCounter, exampleString } = useContext(SecondContext)
+
+  return (
+    <div>
+      <p>{exampleCounter}</p>
+      <p>{exampleString}</p>
+      <footer>Футер</footer>
+    </div>
+  )
+}
+
 function App() {
+  const [conter, setCounter] = useState(0)
+
+  const inctrement = () => {
+    setCounter(prev => prev + 1)
+  }
+
+  const dicrement = () => {
+    setCounter(prev => prev - 1)
+  }
+
+  const exampleCounter = 29;
+  const exampleString = 'Что угодно могу положить'
+
+  const valueForContext1 = {
+    inctrement,
+    dicrement: dicrement,
+    conter: conter,
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <FirstContext.Provider value={valueForContext1}>
+        <Header />
+      </FirstContext.Provider>
 
-      </header>
+      <SecondContext.Provider
+        value={{ exampleCounter, exampleString }}
+      >
+        <Footer />
+      </SecondContext.Provider>
     </div>
   );
 }
